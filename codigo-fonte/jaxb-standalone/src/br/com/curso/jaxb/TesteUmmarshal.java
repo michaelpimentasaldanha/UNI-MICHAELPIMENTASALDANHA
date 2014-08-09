fdcec0baed42815bf728c475adae7b7ac0e7116b
michaelpimentasaldanha <michaelsaldanha@live.com>
2014-08-09 12:19:32 -0300

    v.0.1 - funcionando

--- codigo-fonte/jaxb-standalone/src/br/com/curso/jaxb/TesteUmmarshal.java
+++ codigo-fonte/jaxb-standalone/src/br/com/curso/jaxb/TesteUmmarshal.java
@@ -1,8 +1,6 @@
 package br.com.curso.jaxb;
 
 import java.io.File;
-import java.rmi.UnmarshalException;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -15,5 +13,7 @@ public class TesteUmmarshal {
 		
 		File arquivo = new File("camisa.xml");
 		Produto produto = (Produto) unmarshaller.unmarshal(arquivo);
+	//	System.out.println(produto.getNome()+" - "+produto.getCategoria().getNome());
+		System.out.println("Nome:"+produto.getNome()+"\nPreço: "+produto.getPreco()+"\nCategoria: "+produto.getCategoria().getNome());
 	}
 }
